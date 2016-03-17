f06b0f5fc6538d77c77870578b3caf0490c39cc4
LangZhai <zl2012xyz@hotmail.com>
2016-03-17 17:09:53 +0800

    Update to 1.4.1

--- dist/js/jquery.zldialog.l18n.min.js
+++ dist/js/jquery.zldialog.l18n.min.js
@@ -1,7 +1,7 @@
 /**
  * ZLDialog 1.4.1
  * Date: 2016-03-17
- * © 2013-2016 智能小菜菜
+ * © 2013-2016 LangZhai(智能小菜菜)
  * This is licensed under the GNU LGPL, version 3 or later.
  * For details, see: http://www.gnu.org/licenses/lgpl.html
  */
