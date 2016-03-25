6dcbae1e5ed267bf61b8af51b09fdf1f73a4fe63
LangZhai <zl2012xyz@hotmail.com>
2016-03-25 14:18:16 +0800

    Update to 1.4.5

--- dist/js/jquery.zldialog.js
+++ dist/js/jquery.zldialog.js
@@ -1,12 +1,15 @@
 /**
- * ZLDialog 1.4.4
- * Date: 2016-03-23
+ * ZLDialog 1.4.5
+ * Date: 2016-03-25
  * © 2013-2016 LangZhai(智能小菜菜)
  * This is licensed under the GNU LGPL, version 3 or later.
  * For details, see: http://www.gnu.org/licenses/lgpl.html
  * Project home: https://github.com/LangZhai/ZLDialog
  *
  * ==========更新历史==========
+ * -2016-03-25    1.4.5-
+ *  1.【Update】重构部分代码。
+ *
  * -2016-03-23    1.4.4-
  *  1.【Debug】修复IE8中遮罩层样式异常的BUG；
  *  2.【Debug】修复预览面板鼠标事件异常的BUG；
--- dist/js/jquery.zldialog.l18n.js
+++ dist/js/jquery.zldialog.l18n.js
@@ -1,6 +1,6 @@
 /**
- * ZLDialog 1.4.4
- * Date: 2016-03-23
+ * ZLDialog 1.4.5
+ * Date: 2016-03-25
  * © 2013-2016 LangZhai(智能小菜菜)
  * This is licensed under the GNU LGPL, version 3 or later.
  * For details, see: http://www.gnu.org/licenses/lgpl.html
